f57427c27775baabe55122440e571f35c05e4009
lei286394973 <286394973@qq.com>
2014-08-17 12:16:21 +0800

    投票

--- vote.py
+++ vote.py
@@ -1,11 +1,8 @@
 #coding: utf-8
 
-import requests, datetime, time, Queue, gevent, random, ConfigParser
+import requests, datetime, time, Queue, gevent, random, ConfigParser, random
+from urllib import quote
 from pyquery import PyQuery as pq
-
-import gevent.monkey
-gevent.monkey.patch_socket()
-
 
 class ProxyVote(object):
 
@@ -13,6 +10,9 @@ class ProxyVote(object):
         '''
         初始化
         '''
+        import gevent.monkey
+        gevent.monkey.patch_socket()
+
         self.proxyQueue = Queue.Queue()
         self.okQueue = Queue.Queue()
         self.errQueue = Queue.Queue()
@@ -24,10 +24,10 @@ class ProxyVote(object):
             "min_delay": 5,
             "max_delay": 15,
             "timeout": 15,
-            "success_flag": ""
+            "success_flag": "",
+            "floop_count": 2,
+            "total_count": 100
         }
-        
-        
     
     
     def start(self):
@@ -37,13 +37,18 @@ class ProxyVote(object):
         # 初始化参数
         self.get_options()
         
-        # 拿到代理
-        self.get_proxies()
+        # for i in range(self.options["floop_count"]):
+        while self.okQueue.qsize() <= self.options["total_count"]:
+            # 拿到代理
+            self.get_proxies()
+            
+            # 使用gevent投票
+            self.gevent_vote()
+            
+            self.proxyQueue = Queue.Queue()
+            self.proxies = []
         
-        # 使用gevent投票
-        self.gevent_vote()
-        
-        print u"成功[ %s ]次, 失败[ %s ]次!" % (self.okQueue.qsize(), self.errQueue.qsize()) 
+        print u"成功[ %s ]次, 失败[ %s ]次!" % (self.okQueue.qsize(), self.errQueue.qsize())
     
     
     def get_options(self):
@@ -54,14 +59,38 @@ class ProxyVote(object):
         config.read("vote.ini")
         
         self.options.update({
-            "vote_url": config.get("default", "vote_url"),
+            "vote_url": config.get("default", "vote_url").replace('TIME', str(random.randrange(1000000, 9999999))),
             "proxy_urls": config.get("default", "proxy_urls").split('|'),
             "max_thread": int(config.get("default", "max_thread")),
             "min_delay": int(config.get("default", "min_delay")),
             "max_delay": int(config.get("default", "max_delay")),
             "timeout": int(config.get("default", "timeout")),
-            "success_flag": config.get("default", "success_flag")
+            "success_flag": config.get("default", "success_flag"),
+            "floop_count": int(config.get("default", "floop_count")),
+            "total_count": int(config.get("default", "total_count"))
         })
+    
+    
+    def get_proxy1(self, url):
+        '''
+        获取代理
+        '''
+        # 抓取代理地址
+        req = requests.get(
+            url,
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+            }
+        )
+        
+        # 解析结果
+        dom = pq(req.text)
+        
+        # 填充代理
+        for x in dom('.table').eq(0).find('li.proxy'):
+            
+            self.proxies.append(x.text.strip())
+            self.proxyQueue.put(x.text.strip())
     
     
     def get_proxy(self, url):
@@ -69,15 +98,19 @@ class ProxyVote(object):
         获取代理
         '''
         # 抓取代理地址
-        req = requests.get(url)
-        
-        # 解析结果
-        dom = pq(req.text)
+        req = requests.get(
+            url,
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+            }
+        )
+       
         
         # 填充代理
-        for x in dom('.cont_font').find('p').html().split('<br />&#13;'):
-            self.proxies.append(x.split('@')[0].strip())
-            self.proxyQueue.put(x.split('@')[0].strip())
+        for x in req.text.split('\r\n'):
+            
+            self.proxies.append(x.strip())
+            self.proxyQueue.put(x.strip())
         
 
     def get_proxies(self):
@@ -85,7 +118,11 @@ class ProxyVote(object):
         批量获取代理
         '''        
         for url in self.options["proxy_urls"]:
-            self.get_proxy(url)
+            try:
+                time.sleep(2)
+                self.get_proxy(url)
+            except Exception, e:
+                print e
 
         print u'共获取到[ %s ]个代理' % len(self.proxies)
         
@@ -94,15 +131,83 @@ class ProxyVote(object):
         '''
         投票日志
         '''
-        total = self.okQueue.qsize() + self.errQueue.qsize()
+        msg = u"成功"
         
         if success:
             self.okQueue.put(proxy)
-            print u'%s-哦也, 使用代理[ %s ]成功!!' % (total, proxy)
         else:
             self.errQueue.put(proxy)
-            print u'%s-靠, 使用代理[ %s ]失败!!' % (total, proxy)
+            msg = u"失败"
+        
+        print u'[ %s - %s - %s ]:使用代理[ %s ]%s!![%s]' % (self.okQueue.qsize(), self.errQueue.qsize(), self.okQueue.qsize() + self.errQueue.qsize(), proxy, msg, self.options["vote_url"].decode('utf-8'))
     
+    
+    def vote(self, proxy):
+        '''
+        '''
+        
+        # 间隔秒数
+        time.sleep(
+            random.randint(
+                self.options["min_delay"], 
+                self.options["max_delay"]
+            )
+        )
+        
+        try:
+            # 代理投票
+            req = requests.get(
+                self.options["vote_url"], 
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}, 
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+                }, 
+                timeout = self.options["timeout"]
+            )
+            cookies = req.cookies
+            
+            # 是否成功
+            if req.text.find(self.options["success_flag"]) > -1:
+                # 成功之后 模拟用户点击
+                target_url = ''
+                dom = pq(req.text)
+                result = dom('#results .result')
+                for i in range(len(result)):
+                    
+                    if result.eq(i).find('span.site').text().find(self.options["success_flag"]) > -1:
+                        target_url = result.eq(i).find('a.result_title').attr('href')
+                        break
+                
+                # 百度出的结果有时会去掉 http: 此处做下判断
+                target_url = target_url if target_url.startswith('http') else ("http:"+target_url)
+                # print target_url
+                #time.sleep(1)
+                
+                req = requests.get(
+                    target_url, 
+                    proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}, 
+                    headers = {
+                        'Referer': self.options["vote_url"],
+                        'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+                    }, 
+                    cookies = cookies,
+                    timeout = 15
+                )
+                
+                if req.text.find(self.options["success_flag"]) > -1:
+                    self.vote_log(proxy, True)
+                    return True
+                else:
+                    self.vote_log(proxy, False)
+                    return False
+            else:
+                self.vote_log(proxy, False)
+                return False
+                
+        except Exception, e:
+            # print e
+            self.vote_log(proxy, False)
+            return False
     
     def gevent_vote(self):
         '''
@@ -112,39 +217,19 @@ class ProxyVote(object):
             print '线程[ %s ]开启...' % tid
             
             while count < max_count:
-            
-                # 间隔秒数
-                time.sleep(
-                    random.randint(
-                        self.options["min_delay"], 
-                        self.options["max_delay"]
-                    )
-                )
-                
-                count += 1
                 proxy = self.proxyQueue.get()
+                count += 1
                 
-                try:
-                    # 代理投票
-                    req = requests.get(
-                        self.options["vote_url"], 
-                        proxies = {'http': 'http://%s' % proxy}, 
-                        headers = {
-                            'Referer': 'http://www.baidu.com',
-                            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-                        }, 
-                        timeout = self.options["timeout"]
-                    )
+                if self.vote(proxy):
+                    print '追加一次'
+                    time.sleep(2)
                     
-                    # 是否成功
-                    if req.text.find(self.options["success_flag"]) > -1:
-                        self.vote_log(proxy, True)
-                    else:
-                        self.vote_log(proxy, False)
-                        
-                except Exception, e:
-                    # print e
-                    self.vote_log(proxy, False)
+                    if self.vote(proxy):
+                        print '再追加一次'
+                        time.sleep(2)
+                        self.vote(proxy)
+                        pass
+                
                     
         
         # gevent配置
---
+++ zhixuan.py
@@ -0,0 +1,191 @@
+#coding: utf-8
+import requests, datetime, time, Queue, gevent, random, ConfigParser, random
+from urllib import quote
+from pyquery import PyQuery as pq
+
+import gevent.monkey
+gevent.monkey.patch_socket()
+
+class ProxyVote(object):
+    
+    def __init__(self, name, target_count, vote_url, proxy_urls, flag):
+        '''
+        '''
+        self.name = name
+        self.target_count = random.randint(750, 1300) # target_count
+        self.vote_url = vote_url
+        self.proxy_urls = proxy_urls
+        self.flag = flag
+        
+        self.proxies = []
+        self.total = 0
+        self.success = 0
+        self.fail = 0
+    
+    
+    def get_proxies(self):
+        '''
+        获取代理
+        '''
+        self.proxies = []
+        
+        for url in self.proxy_urls:
+            
+            gevent.sleep(1.2)
+            
+            try:
+                # 抓取代理地址
+                req = requests.get(
+                    url,
+                    headers = {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
+                    }
+                )
+                
+                # 解析结果
+                dom = pq(req.text)
+                
+                # 填充代理
+                for x in dom('.table').eq(0).find('li.proxy'):
+                    
+                    self.proxies.append(x.text.strip())
+            except Exception, e:
+                print e
+        
+        print u"[ %s ]抓取到[ %s ]个代理" % (self.name, len(self.proxies))
+    
+    
+    def vote_log(self, proxy, flag):
+        self.total += 1
+        msg = u"成功!!"
+        
+        if flag:
+            self.success += 1
+            print u"\033[1;32;40m [ %s - %s - %s - %s ]使用[ %s ]代理%s \033[0m" % (self.name, self.success, self.fail, self.total, proxy, msg)
+        else:
+            self.fail += 1
+            msg = u"失败"
+            print u"\033[1;31;40m [ %s - %s - %s - %s ]使用[ %s ]代理%s \033[0m" % (self.name, self.success, self.fail, self.total, proxy, msg)
+        
+    
+    
+    def _vote(self, proxy):
+        try:
+            gevent.sleep(random.randint(4, 8))
+            
+            # 首先访问百度的页面主要是获取cookies
+            req = requests.get(
+                self.vote_url, 
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}, 
+                headers = {
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+                }, 
+                timeout = 15
+            )
+            cookies = req.cookies
+            
+            # 找出要点击的url
+            target_url = ''
+            dom = pq(req.text)
+            result = dom('#results .result')
+            for i in range(len(result)):
+                
+                if result.eq(i).find('span.site').text().find(self.flag) > -1:
+                    target_url = result.eq(i).find('a.result_title').attr('href')
+                    break
+                    
+            # 百度出的结果有时会去掉 http: 此处做下判断
+            target_url = target_url if target_url.startswith('http') else ("http:"+target_url)
+            
+            #print target_url
+            
+            # 访问目标url
+            req = requests.get(
+                target_url, 
+                proxies = {'http': 'http://%s' % proxy, 'https': 'http://%s' % proxy}, 
+                headers = {
+                    'Referer': self.vote_url,
+                    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 7_0 like Mac OS X; en-us) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
+                }, 
+                cookies = cookies,
+                timeout = 15
+            )
+            
+            # 检测访问是否成功
+            if req.text.find(self.flag) > -1:
+                self.vote_log(proxy, True)
+                return True
+            else:
+                self.vote_log(proxy, False)
+                return False
+            
+        except Exception, e:
+                #print e
+                self.vote_log(proxy, False)
+                return False
+        
+        
+        
+    def vote(self):
+    
+        # 获取代理
+        self.get_proxies()
+        
+        for proxy in self.proxies:
+            if self._vote(proxy):
+                print u'追加一次'
+                if self._vote(proxy):
+                    print u'再追加一次'
+                
+    
+    def start(self):
+        '''
+        '''
+        print u'-------------------开始处理[ %s ]的投票-------------------' % self.name
+        gevent.sleep(random.randint(10, 200))
+        # 循环开始投票
+        while self.success < self.target_count:
+            self.vote()
+        
+        print u'[ %s ]的投票结果: 成功 [ %s ], 失败 [ %s ], 总次数[ %s ]' % (self.name, self.success, self.fail, self.total)
+        print u'-------------------处理[ %s ]的投票结束-------------------' % self.name
+    
+    
+    
+    
+        
+        
+        
+if __name__ == "__main__":
+
+    print u'开始...'
+
+    config = ConfigParser.SafeConfigParser()
+    config.read("zhixuan.ini")
+        
+    threads = []
+    for section in config.sections():
+        
+        threads.append(
+            gevent.spawn(
+                ProxyVote(
+                    section.decode('utf-8'), 
+                    int(config.get(section, "total_count")),
+                    config.get(section, "vote_url").replace('TIME', str(random.randrange(1000000, 9999999))),
+                    config.get(section, "proxy_urls").split('|'),
+                    config.get(section, "success_flag")
+                ).start
+            )
+        )
+    
+    gevent.joinall(threads)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
