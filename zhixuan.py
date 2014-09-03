3b0afbdd5348396c90d7c13a7ad94894a12070f2
lei286394973 <286394973@qq.com>
2014-09-03 13:57:22 +0800

    cpu

--- zhixuan.py
+++ zhixuan.py
@@ -147,7 +147,7 @@ class ProxyVote(object):
         gevent.sleep(random.randint(10, 200))
         # 循环开始投票
         while (datetime.datetime.now() - self.start_time).total_seconds() < self.total_hour*3600:
-            time.sleep(0.1)
+            time.sleep(0.5)
             if self.success < self.target_count:
                 self.vote()
             else:
