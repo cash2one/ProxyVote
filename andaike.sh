6b6dec35e44404745757963ff6a053e7304147be
lei286394973 <286394973@qq.com>
2014-09-03 17:03:23 +0800

    chmod

--- andaike.sh
+++ andaike.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/opt/python2.7.2/bin/python /var/www/ProxyVote/andaike_t.py>/var/www/ProxyVote/andaike.log
+/opt/python2.7.2/bin/python /var/www/ProxyVote/a.py>/var/www/ProxyVote/andaike.log
